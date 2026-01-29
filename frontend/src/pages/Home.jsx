85f2d3c5db6ebe3c55d4d4da089119ced9fee9c2
Adriana <adry.borja.1995@gmail.com>
2026-01-29 12:28:14 -0500

    Cambios en pagina Home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,17 @@
 import { Typography, Box } from "@mui/material";
+import MenuBookIcon from "@mui/icons-material/MenuBook";
 
 const Home = () => {
   return (
     <Box textAlign="center">
+      <MenuBookIcon
+        sx={{
+          fontSize: 200,
+          mb: 2,
+          color: "secondary.main",
+        }}
+      />
+
       <Typography variant="h4" gutterBottom>
         Bienvenido al sistema editorial
       </Typography>
